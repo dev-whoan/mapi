8ebb2ccbfec6f478c7782322149ac0f9de9ef9cc
김민환 <eugene@Eugene-Studio.local>
2022-07-23 16:29:49 +0900

    Initailize Pod

--- core/enum/apiType.js
+++
@@ -1,8 +0,0 @@
-const API_TYPE = {
-    GENERAL: "general",
-    REST: "restapi",
-    JWT: "jwt",
-    MODEL: "model"
-};
-
-export default API_TYPE;
--- core/enum/dbCrud.js
+++
@@ -1,8 +0,0 @@
-const DB_CRUD = {
-    INSERT: 'insert',
-    SELECT: 'select',
-    UPDATE: 'update',
-    DELETE: 'delete'
-};
-
-export default DB_CRUD;
